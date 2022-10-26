0cfb7033074b7e9fc51e4a27d15a535d3d645b57
Max Ag <90968222+maxstrb@users.noreply.github.com>
2022-10-26 20:40:23 +0200

    Update main.py

--- Pn/main.py
+++ Pn/main.py
@@ -1,24 +1,25 @@
 # optimized - 100% neopisujte, tohle by opravdu poznal, na vlastní nebezpečí
-def f1(Rudofl: int): # neboli golf
-    # prime numbers up to and including 'Rudofl'
+def f1(Rudolf: int): # neboli golf
+    # prime numbers up to and including 'Rudolf'
     
     # Handling very funny people
-    if type(Rudofl) != int:
+    if type(Rudolf) != int:
         raise TypeError("Entry to this function must be an int")
     
-    if Rudofl < 2:
+    if Rudolf < 2:
         return []
     # End of handling very funny people
     
-    Rudofl += 1
-    MNL = [True] * Rudofl
-    MNL[0] = MNL[1] = False
+    Rudolf += 1 # Rudolf je moc malý, takže ho zvětšuji
+    MNL = [True] * Rudolf # MNL = hodněkrát True, přesněji Rudoflkrát True
+    MNL[0] = MNL[1] = False # Tyhle dva nechci
     
-    for René in range(2, int(Rudofl**0.5) + 1):
-        if MNL[René]:
-            for Josef in range(René * René, Rudofl, René):
-                MNL[Josef] = False
-                
+    for René in range(2, int(Rudolf**0.5) + 1): # Blbý for cyklus, ale funguje
+        if MNL[René]: # Pokud je René prvočíslo tak
+            for Josef in range(René * René, Rudolf, René): # Všechna čísla, která jsou dělitelná René, jsou neprvočísla
+                MNL[Josef] = False # Takže je nastavím na False
+    
+    # Výstup je seznam prvočísel, od 2 do Rudolfa
     return [René for René, Já in enumerate(MNL) if Já]
 
 # větší šance na, že nepozná okopírování když se to trochu změní
